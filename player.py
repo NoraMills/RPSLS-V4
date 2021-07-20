a1b754c510aef2224d34c009eb204e0f786712fc
Nora Mills <82294371+NoraMills@users.noreply.github.com>
2021-07-20 12:54:39 -0500

    Add files via upload

---
+++ player.py
@@ -0,0 +1,9 @@
+class Player:
+
+    gesture_list = ["rock", "paper", "scissors", "lizard", "spock"]
+
+    def __init__(self, name):
+        self.name = ""
+
+    def select_gesture(self):
+        return "Gesture"
